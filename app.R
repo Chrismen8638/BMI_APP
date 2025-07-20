832c2f80233b25cf6b0baa47b9bd101db5fe50a1
Chrismen8638 <144564162+Chrismen8638@users.noreply.github.com>
2025-07-20 10:27:48 +0300

    Update app.R

--- app.R
+++ app.R
@@ -1,51 +1,77 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
+setwd("C:/Users/User/Desktop/BMI")
 
-library(shiny)
+## Tracking BMI Application
+install.packages("shiny")
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
+install.packages("shiny", dependencies = TRUE)
+install.packages("Rcpp")
+require(shiny)
 
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
 
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
+ui = fluidPage(
+  titlePanel("BMI Calculator"),
+  sidebarLayout(
+    sidebarPanel(
+      h4("Enter Your Measurements"),
+      numericInput("weight", "Weight (kg):", value = 70, min = 1, max = 300),
+      numericInput("height", "Height (m):", value = 1.75, min = 0.1, max = 3.0),
+      actionButton("calculate", "Calculate BMI")
+    ),
+    mainPanel(
+      h4("Results"),
+      textOutput("bmi_result"),
+      textOutput("bmi_category")
     )
+  )
 )
 
-# Define server logic required to draw a histogram
+# server logic
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+  # Reactive expression to calculate BMI when the button is clicked
+  bmi <- eventReactive(input$calculate, {
+    weight <- input$weight
+    height <- input$height
+    if (height > 0) {
+      bmi_value <- weight / (height^2)
+      round(bmi_value, 5) # Round to 5 decimal places
+    } else {
+      NA # Return NA if height is 0 or negative
+    }
+  })
+  
+  
+  output$bmi_result <- renderText({
+    if (is.na(bmi())) {
+      "Please enter a valid height (greater than 0)."
+    } else {
+      paste("Your BMI is:", bmi())
+    }
+  })
+  
+  
+  output$bmi_category <- renderText({
+    if (is.na(bmi())) {
+      ""
+    } else {
+      bmi_value <- bmi()
+      if (bmi_value < 18.5) {
+        "Category: Underweight"
+      } else if (bmi_value >= 18.5 && bmi_value < 25.0) {
+        "Category: Normal weight"
+      } else if (bmi_value >= 25.0 && bmi_value < 30.0) {
+        "Category: Overweight"
+      } else {
+        "Category: Obese"
+      }
+    }
+  })
 }
 
-# Run the application 
+
 shinyApp(ui = ui, server = server)
+
+
+install.packages("rsconnect")
+
+library(rsconnect)
+rsconnect::deployApp("C:/Users/User/Desktop/BMI")
